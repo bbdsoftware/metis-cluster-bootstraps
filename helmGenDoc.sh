9b9828d036cbe01d750c230635bf22b0dbd654d0
Ricardo Pinto <pinto.ricky@gmail.com>
2021-07-28 09:55:00 +0100

    Create chart doc dir if doesnt exisit

--- helmGenDoc.sh
+++ helmGenDoc.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ ! -d ./docs/charts/modules/Chart-Docs ]; then
+  mkdir -p ./docs/charts/modules/Chart-Docs;
+fi
+
 for f in ./charts-modules/*; do
     if [ -d "$f" ]; then
         # Will not run if no directories are available

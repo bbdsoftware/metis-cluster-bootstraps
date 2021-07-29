486a64e7443f450bc508c94c740576f1da701655
Ricardo Pinto <pinto.ricky@gmail.com>
2021-07-29 10:19:30 +0100

    Various documentation fixes (#42)

--- helmGenDoc.sh
+++ helmGenDoc.sh
@@ -7,8 +7,26 @@ fi
 for f in ./charts-modules/*; do
     if [ -d "$f" ]; then
         # Will not run if no directories are available
-        echo "$f"
         filename=`basename $f`
+        echo "Generating readme for ./docs/charts/modules/Chart-Docs/$filename.md"
         frigate gen $f > ./docs/charts/modules/Chart-Docs/$filename.md
+        echo "Generating readme for .$f/README.md"
+        frigate gen $f > $f/README.md
+    fi
+done
+
+
+if [ ! -d ./docs/charts/bootstrap/Chart-Docs ]; then
+  mkdir -p ./docs/charts/bootstrap/Chart-Docs;
+fi
+
+for f in ./charts-bootstrap/*; do
+    if [ -d "$f" ]; then
+        # Will not run if no directories are available
+        filename=`basename $f`
+        echo "Generating readme for ./docs/charts/bootstrap/Chart-Docs/$filename.md"
+        frigate gen $f > ./docs/charts/bootstrap/Chart-Docs/$filename.md
+        echo "Generating readme for .$f/README.md"
+        frigate gen $f > $f/README.md
     fi
 done

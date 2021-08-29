c45f9ed2b127a2534de3f411125e80e5e0122d30
Ricardo Pinto <pinto.ricky@gmail.com>
2021-08-29 21:03:13 +0100

    Adding role creation to boostrap.sh

--- charts-bootstrap/boostrap.sh
+++ charts-bootstrap/boostrap.sh
@@ -1,3 +1,50 @@
+set -e
+
+IAM_ROLE_NAME=metis-seed
+AWS_ACCOUNT_ID=$1
+CLUSTER=$2
+SERVICE_ACCOUNT_NAMESPACE=crossplane-system
+OIDC_PROVIDER=$(aws eks describe-cluster --name "$CLUSTER" --region eu-west-1  --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+
+echo AWS_ACCOUNT_ID: $AWS_ACCOUNT_ID
+echo IAM_ROLE_NAME: $IAM_ROLE_NAME
+echo SERVICE_ACCOUNT_NAMESPACE : $SERVICE_ACCOUNT_NAMESPACE
+
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+    exit 0
+fi
+
+echo creating iam
+
+read -r -d '' TRUST_RELATIONSHIP <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::${AWS_ACCOUNT_ID}:oidc-provider/${OIDC_PROVIDER}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringLike": {
+          "${OIDC_PROVIDER}:sub": "system:serviceaccount:${SERVICE_ACCOUNT_NAMESPACE}:provider-aws-*"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+echo "${TRUST_RELATIONSHIP}" > trust.json
+aws iam create-role --role-name $IAM_ROLE_NAME --assume-role-policy-document file://trust.json --description "IAM role for provider-aws"
+aws iam attach-role-policy --role-name $IAM_ROLE_NAME --policy-arn=arn:aws:iam::aws:policy/AdministratorAccess
+rm -rf trust.json
+
+
 kubectl create namespace crossplane-system
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
@@ -11,7 +58,7 @@ helm upgrade --namespace crossplane-system metis ./metis-crossplane-bootstrap --
 echo "Waiting on providers installs.."
 kubectl  wait --for condition=established --timeout=60s crd/providerconfigs.helm.crossplane.io
 kubectl  wait --for condition=established --timeout=60s crd/providerconfigs.kubernetes.crossplane.io
-helm upgrade --namespace crossplane-system metis-config ./metis-crossplane-bootstrap-config --install
+helm upgrade --namespace crossplane-system metis-config ./metis-crossplane-bootstrap-config --install --set spec.aws.iamServiceAccounts.iamRoleArn=arn:aws:iam::$AWS_ACCOUNT_ID:role/$IAM_ROLE_NAME
 
 
 SA=$(kubectl -n crossplane-system get sa -o name | grep provider-kubernetes | sed -e 's|serviceaccount\/|crossplane-system:|g')
